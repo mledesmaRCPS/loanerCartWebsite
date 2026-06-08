986cde17c751115bca727fdb3c023b44892ed54b
mledesmaRCPS <mledesma@rockingham.k12.va.us>
2026-06-08 14:52:13 -0400

    check 2

--- dataQuester.js
+++ dataQuester.js
@@ -288,7 +288,7 @@ service.patch('/blockLoaners/:loanerNumber', (request, response) => {
     parseInt(request.body.blStatus)
   ];
 
-  const query = 'UPDATE BlockLoaners SET loanerNumber = 1, id = 2002765, serialNumber = "p208vjyq", blStatus = 1 WHERE loanerNumber = ?';
+  const query = 'UPDATE BlockLoaners SET loanerNumber = 1, id = 2002765, serialNumber = "p208vjyq", blStatus = ? WHERE loanerNumber = ?';
   connection.query(query, parameters, (error, result) => {
     if (error) {
       response.status(404);
