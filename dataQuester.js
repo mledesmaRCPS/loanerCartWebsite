fda299382df509b034124cc7f52af8e40f93880c
mledesmaRCPS <mledesma@rockingham.k12.va.us>
2026-06-22 16:03:35 -0400

    trying to go back

--- dataQuester.js
+++ dataQuester.js
@@ -335,7 +335,7 @@ service.post('/formResponses', (request, response) => {
     response.status(400);
     response.json({
       ok: false,
-      results: result.blStatus,
+      results: "SOMETHING IS WRONG, CHECK THE DATA",
     });
   }
 });
